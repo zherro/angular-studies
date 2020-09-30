c8e94bbe98d5cfdecb5c7f85c3232696bb56106b
cleiton.oliveira <cleiton.ti@outlook.com>
2020-09-29 21:49:00 -0400

    registro de tokem do no localstorage

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { TokenService } from '../toke/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -9,10 +11,16 @@ const API_URL = 'http://localhost:3000';
 export class AuthService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private token: TokenService
     ) { }
 
   authenticate(userName: string, password: string){
-    return this.http.post(API_URL + '/user/login', {userName, password})
+    return this.http
+      .post(API_URL + '/user/login', {userName, password}, {observe: 'response'})
+      .pipe(tap(res => {
+          const authToken = res.headers.get('x-access-token');
+          this.token.setToken(authToken);
+      }));
   }
 }
---
+++ src/app/core/toke/token.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'authToken';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+
+  hasToken() {
+    return !!this.getToken();
+  }
+
+  setToken(token){
+    window.localStorage.setItem(KEY, token);
+  }
+
+  getToken() {
+    return window.localStorage.getItem(KEY);
+  }
+
+  removeToken() {
+    window.localStorage.removeItem(KEY);
+  }
+
+}
